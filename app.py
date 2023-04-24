5b38806dc4b657e752a98bff45945bbeeaceda2c
FareedKhan-dev <whereismymind557@gmail.com>
2023-04-24 18:09:46 +0500

    the_final_work

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request
-import config
-import aiapi
-import pandas as pd
+import os
 import numpy as np
-# from openai.embeddings_utils import get_embedding
+import openai
+from openai.embeddings_utils import get_embedding
 from sklearn.metrics.pairwise import cosine_similarity
 
 import gzip
+COMPLETIONS_MODEL = "text-davinci-003"
+openai.api_key = os.getenv('my_new_api_key_here')
+
 
 with gzip.open('vectorized_data.npy.gz', 'rb') as f:
     data_array = np.load(f, allow_pickle=True)
@@ -16,21 +18,54 @@ def page_not_found(e):
 
 
 app = Flask(__name__)
-app.config.from_object(config.config['development'])
-
-app.register_error_handler(404, page_not_found)
-
 
 @app.route('/', methods = ['POST', 'GET'])
 def index():
     
-    if request.method == 'POST':
-      prompt = request.form['prompt']
-      answer = aiapi.get_response_of_text(prompt)
-      return answer
+  if request.method == 'POST':
+    prompt = request.form['prompt']
+    question = prompt
+    question_vector = get_embedding(question, engine='text-embedding-ada-002')
+    question_vector_transform = np.array(question_vector).reshape(1, -1)
+    similarity = []
 
-    return render_template('index.html', **locals())
+    for each in data_array[0]:
+        v2 = np.array(each).reshape(1, -1)
+        # compute the cosine similarity
+        cosinesimilarity = cosine_similarity(question_vector_transform, v2)[0][0]
+        similarity.append(cosinesimilarity)
+
+    similarity = np.array(similarity)
+    
+    # Get the sorted indices of y in descending order
+    sorted_indices = np.argsort(-similarity)
+
+    # Sort x and y based on sorted_indices
+    information_this = ' '.join(data_array[1][sorted_indices[0:2]])
+    # cosinesimilarity_this = similarity[sorted_indices[0]]
+
+    prompt = f""" Answer only IBA related question using only the context below. If someone asked about your information reply to them exact text "I am an IBA CHATBOT, who solve your queries", Answer it in professional way, if you are not able to answer the question make sure you don't give any wrong information
+
+    Context:
+    {information_this}
+
+    Q: {question}
+    A:"""
+
+    answer = openai.Completion.create(
+        prompt=prompt,
+        temperature=1,
+        max_tokens=500,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        model=COMPLETIONS_MODEL
+    )["choices"][0]["text"].strip(" \n")
+
+    return answer
+
+  return render_template('index.html', **locals())
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
